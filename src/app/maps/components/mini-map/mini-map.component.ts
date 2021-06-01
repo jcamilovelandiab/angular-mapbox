f86c4983104fa9db29d4126c3a778ce1bb8dcd4e
Camilo <jcamilovelandiab@gmail.com>
2021-05-31 21:38:53 -0500

    Finishing properties page component and mini-map component.

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,16 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-mini-map',
   templateUrl: './mini-map.component.html',
-  styles: [
-  ]
+  styles: [`
+    div {
+      width: 100%;
+      height: 150px;
+      margin: 0;
+    }
+  `]
 })
-export class MiniMapComponent implements OnInit {
+export class MiniMapComponent implements AfterViewInit {
+
+  @Input() lngLat: [ number, number ] = [ 0, 0];
+
+  @ViewChild('map') divMap!: ElementRef;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    var map = new mapboxgl.Map({
+      container: this.divMap.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.lngLat,
+      zoom: 15,
+      interactive: false
+    });
+
+    new mapboxgl.Marker()
+        .setLngLat( this.lngLat )
+        .addTo( map );
+
   }
 
 }
