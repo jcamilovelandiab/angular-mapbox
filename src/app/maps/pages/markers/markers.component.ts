ea7bb4f8a7ffc320424443bc7e282aa04cf7b0e4
Camilo <jcamilovelandiab@gmail.com>
2021-05-31 20:27:23 -0500

    Creating a simple marker in the map

--- src/app/maps/pages/markers/markers.component.ts
+++ src/app/maps/pages/markers/markers.component.ts
@@ -1,16 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-markers',
   templateUrl: './markers.component.html',
-  styles: [
-  ]
+  styles: [`
+    .map-container {
+      height: 100%;
+      width: 100%;
+    }
+  `]
 })
-export class MarkersComponent implements OnInit {
+export class MarkersComponent implements AfterViewInit {
+
+  @ViewChild('map') divMap!: ElementRef;
+
+  map!: mapboxgl.Map;
+  zoomLevel: number = 15;
+  center: [ number, number ] = [ -74.03773325096206, 4.753235974221012 ];
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.map = new mapboxgl.Map({
+      container: this.divMap.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.center,
+      zoom: this.zoomLevel
+    });
+
+    const marker = new mapboxgl.Marker()
+        .setLngLat( this.center )
+        .addTo( this.map );
+
   }
 
 }
